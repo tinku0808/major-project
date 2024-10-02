653cd7f7f648c4b0d916f94de81b92c831f7a782
Sabareeshwaran M <tinku.sabarish88@gmail.com>
2024-10-02 20:31:30 +0530

    Packages Updated

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer"); // Add Nodemailer import
 const User = require("./models/User");
 const authRoutes = require("./routes/authRoutes");
 const learningMaterialRoutes = require("./routes/learningMaterialRoutes");
@@ -59,3 +60,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
